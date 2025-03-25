6f5abc4a31a13e34dedfa1356350601d145d26fa
pramod-79 <pramodlodhi645@gmail.com>
2025-03-25 14:19:15 +0530

    Python Basics

--- python_pratice_code/class_statement/basic_class_obj.py
+++ python_pratice_code/class_statement/basic_class_obj.py
@@ -1,5 +1,7 @@
-# The class `Car` defines a blueprint for creating car objects with attributes for brand and model,
-# and an instance `my_car` is created with brand "BMW" and model "M5".
+"""
+The class `Car` defines a blueprint for creating car objects with attributes for brand and model,
+and an instance `my_car` is created with brand "BMW" and model "M5".
+"""
 class Car:
     def __init__(self, brand, model):
         self.brand = brand
